a3e922e548c93a0acd244fc473bc84a7eb312be1
stormerino78 <tachot.adrien7@gmail.com>
2024-03-27 12:43:31 +0100

    Resolving dependencies vulerability issues

--- README.md
+++
@@ -1,3 +0,0 @@
-# CV-Project
-
-The goal is to offer full transparency to the job-seeker in addition of giving a better management tool for employers during application processes with the blockchain
--- frontend/submitCV.js
+++ frontend/submitCV.js
@@ -9,18 +9,29 @@ async function submitCV() {
         // Include other fields accordingly
     };
 
+    //send the data to the API
+    fetch('http://localhost:3000/uploadCV', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(cvData), // Convert cvData to a JSON string
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+
     // Convert cvData to a JSON string
     const jsonData = JSON.stringify(cvData);
-
     // Create a Blob from the JSON string
     const blob = new Blob([jsonData], {type: "application/json"});
-
     // Optionally, convert the Blob to a File
     const file = new File([blob], "cvData.json", {type: "application/json"});
 
-    //console.log(jsonData);
-    //console.log(blob);
+    console.log(jsonData);
     console.log(file);
 
-    uploadToIPFS(file) //sending the data to the IPFS
+    //uploadToIPFS(file) //sending the data to the IPFS
 }
