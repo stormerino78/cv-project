bacead9ff98ed1b618cd37f49f1c6a79559f89e1
stormerino78 <tachot.adrien7@gmail.com>
2024-03-28 17:23:18 +0100

    modifying UpdateSmartContract script to use it with metamask

--- frontend/connectMetamask.js
+++ frontend/connectMetamask.js
@@ -4,12 +4,14 @@ async function connectMetamask() {
             // If MetaMask is not installed, we use the default provider(INFURA), no private keys installed so read-only access
             console.log("MetaMask not installed; using read-only defaults")
             provider = ethers.getDefaultProvider()
+            console.log(provider);
         } else {
             // Connect to the MetaMask EIP-1193 object which allows Ethers access to make all read-only requests through MetaMask.
             const provider = new ethers.BrowserProvider(window.ethereum)
             // Request access to write operations, which will be performed by the private key of the user
             const signer = await provider.getSigner();
-            console.log('Connected to', signer);
+            const address = await signer.getAddress();
+            console.log('Connected to', address);
             return signer;
         }
     } catch (error) {
@@ -17,3 +19,5 @@ async function connectMetamask() {
         return null; // Handle errors or absence of MetaMask
     }   
 }
+
+//exports.connectMetamask = connectMetamask;
--- scripts/UpdateSmartContract.js
+++ scripts/UpdateSmartContract.js
@@ -1,22 +1,15 @@
-const fs = require('fs');
-const path = require('path');
+async function loadABI() { // Get the ABI of the contract
+  const response = await fetch('../artifacts/contracts/digitalCV.sol/digitalCV.json'); // path to the artifact file of the contract
+  const artifact = await response.json();
+  return artifact.abi;
+}
 
-//Get the ABI of the current contract
-const artifactPath = path.join(__dirname, '../artifacts/contracts/digitalCV.sol/digitalCV.json'); // path to the artifact file of the contract
-// Load the JSON data from the artifact file
-const artifact = JSON.parse(fs.readFileSync(artifactPath, 'utf8'));
-// Extract the ABI from the artifact
-const contractABI = artifact.abi;
-
-async function updateSmartContract(contractAddress, ipfsHash, hashIndex) {
-  const [signer] = await ethers.getSigners(); // get the person who will execute the transaction (here the private key referenced in the hardhat environment)
+async function updateSmartContract(contractAddress, ipfsHash, signer, hashIndex = 0) {
+  const contractABI = await loadABI();
   const contract = new ethers.Contract(contractAddress, contractABI, signer); // `signer` should be connected to your wallet
   const tx = await contract.updateCVHash(hashIndex, ipfsHash); //hashIndex correspond to the CV the user wants to modify (if he has several)
   await tx.wait(); // Wait for the transaction to be mined
   console.log(`The hash in smart contract ${contractAddress} has been updated`);
 }
 
-exports.updateSmartContract = updateSmartContract;
-
-//testing purposes
-//updateSmartContract();
+//exports.updateSmartContract = updateSmartContract;
