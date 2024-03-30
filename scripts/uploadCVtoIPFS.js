0aff51ca1b8cd716638b4902906c9d96753d4b0a
stormerino78 <tachot.adrien7@gmail.com>
2024-03-30 14:10:08 +0100

    return IPFS hash correctly and error handling

--- scripts/uploadCVtoIPFS.js
+++ scripts/uploadCVtoIPFS.js
@@ -1,14 +1,11 @@
 //requirements
 require('dotenv').config({ path: '.env' }); // adjust the path to get the variable from the .env file in the parent directory (.env or ../.env)
 const axios = require('axios');
-const FormData = require('form-data');
-const fs = require('fs');
-const {PINATA_API_KEY} = process.env; //get INFURA api key from .env file
+const {PINATA_API_KEY} = process.env; //get Pinata api key from .env file
 
 async function uploadToIPFS(data) {
     /* data form:
     {name: 'hello', email: 'hello.hi@hello.fr', job: 'zaczc', bio: 'zac'}*/ 
-    
     const config = {
         method: 'POST',
         headers: {
@@ -17,17 +14,29 @@ async function uploadToIPFS(data) {
         },
         body: JSON.stringify(data)
     };
-
-    fetch('https://api.pinata.cloud/pinning/pinJSONToIPFS', config)
-        .then(response => response.json())
-        .then(response => console.log(response.IpfsHash))
-        .catch(err => console.error(err));
+    try {
+        const response = await fetch('https://api.pinata.cloud/pinning/pinJSONToIPFS', config);
+        const responseData = await response.json();
+        if(response.ok) {
+            console.log(responseData.IpfsHash);
+            return responseData.IpfsHash; //Returns the hash of the data stored in the IPFS
+        }else {
+            //throw an error
+            console.error("Failed to pin data to IPFS:", responseData.error);
+            throw new Error(responseData.error);
+        }
+    } catch (error) {
+        console.error("Error uploading to IPFS:", error);
+    }
 }
+
+//export it as uploadToIPFS to use it in the run.js
+exports.uploadToIPFS = uploadToIPFS;
+
+
 
 
 /* Test options
-
-
 // Connection Test to pinata API
     
     const config = {
@@ -41,10 +50,9 @@ async function uploadToIPFS(data) {
         .then(response => console.log(response))
         .catch(err => console.error(err));
 
-        
-//export it as uploadToIPFS to use it in the run.js
-exports.uploadToIPFS = uploadToIPFS;
-
 // upload a CV file to IPFS command (test purposes)
 // uploadToIPFS('./uploadToIPFS.jpg').then(ipfsHash => console.log("CV uploaded to IPFS with hash:", ipfsHash));
+
 */
+
+
